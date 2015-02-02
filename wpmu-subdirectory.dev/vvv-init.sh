878834316d899a7b7b136678f80659c501bf996b
Christian Foellmann <foellmann@foe-services.de>
2015-02-02 15:30:01 +0100

    added xip.io support

--- wpmu-subdirectory.dev/vvv-init.sh
+++ wpmu-subdirectory.dev/vvv-init.sh
@@ -24,6 +24,12 @@ if [[ ! -d /srv/www/wpmu-subdirectory ]]; then
 
 	echo "Configuring WordPress Multisite Subdirectory Stable..."
 	wp core config --dbname=wpmu_subdirectory --dbuser=wp --dbpass=wp --quiet --extra-php --allow-root <<PHP
+// Match any requests made via xip.io.
+if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(wpmu-subdirectory.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
+	define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
+	define( 'WP_SITEURL', 'http://' . \$_SERVER['HTTP_HOST'] );
+}
+
 define( 'WP_DEBUG', true );
 PHP
 	echo "Installing WordPress Multisite Subdirectory Stable..."
