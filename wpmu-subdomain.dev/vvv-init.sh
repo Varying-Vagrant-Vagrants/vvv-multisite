6bd98230b4586756f9b22d0d804c10e8bb59a2e8
Shady Sharaf <shady@sharaf.me>
2016-02-04 08:55:13 +0200

    Remove quiet parameter

--- wpmu-subdomain.dev/vvv-init.sh
+++ wpmu-subdomain.dev/vvv-init.sh
@@ -23,7 +23,7 @@ if [[ ! -d /srv/www/wpmu-subdomain ]]; then
 	wp core download
 
 	echo "Configuring WordPress Multisite Subdomain Stable..."
-	wp core config --dbname=wpmu_subdomain --dbuser=wp --dbpass=wp --quiet --extra-php --allow-root <<PHP
+	wp core config --dbname=wpmu_subdomain --dbuser=wp --dbpass=wp --extra-php --allow-root <<PHP
 // Match any requests made via xip.io.
 if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(wpmu-subdomain.)\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}(.xip.io)\z/', \$_SERVER['HTTP_HOST'] ) ) {
 	define( 'WP_HOME', 'http://' . \$_SERVER['HTTP_HOST'] );
@@ -33,13 +33,13 @@ if ( isset( \$_SERVER['HTTP_HOST'] ) && preg_match('/^(wpmu-subdomain.)\d{1,3}\.
 define( 'WP_DEBUG', true );
 PHP
 	echo "Installing WordPress Multisite Subdomain Stable..."
-	wp core multisite-install --allow-root --url=wpmu-subdomain.dev --subdomains --quiet --title="WPMU Subdomain Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password" --allow-root
+	wp core multisite-install --allow-root --url=wpmu-subdomain.dev --subdomains --title="WPMU Subdomain Dev" --admin_name=admin --admin_email="admin@local.dev" --admin_password="password" --allow-root
 
 	# Create sites 2-9
-	wp site create --allow-root --slug=site2 --title="WPMU Subdomain (2)" --email="admin@local.dev" --quiet --allow-root
-	wp site create --allow-root --slug=site3 --title="WPMU Subdomain (3)" --email="admin@local.dev" --quiet --allow-root
-	wp site create --allow-root --slug=site4 --title="WPMU Subdomain (4)" --email="admin@local.dev" --quiet --allow-root
-	wp site create --allow-root --slug=site5 --title="WPMU Subdomain (5)" --email="admin@local.dev" --quiet --allow-root
+	wp site create --allow-root --slug=site2 --title="WPMU Subdomain (2)" --email="admin@local.dev" --allow-root
+	wp site create --allow-root --slug=site3 --title="WPMU Subdomain (3)" --email="admin@local.dev" --allow-root
+	wp site create --allow-root --slug=site4 --title="WPMU Subdomain (4)" --email="admin@local.dev" --allow-root
+	wp site create --allow-root --slug=site5 --title="WPMU Subdomain (5)" --email="admin@local.dev" --allow-root
 
 else
 
